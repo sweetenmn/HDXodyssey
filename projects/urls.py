ee886eafc886f35aee737a92847c8f5c68ac9ad8
Marina Sweeten <sweet.rinaaa@gmail.com>
2016-03-28 15:49:53 -0500

    index

---
+++ projects/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'projects'
+urlpatterns = [url(r'^$', views.IndexView.as_view(), name = 'index'),
+               ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.http import Http404
+from django.views import generic
 
 # Create your views here.
+
+class IndexView(generic.DetailView):
+    template_name = 'projects/index.html'
