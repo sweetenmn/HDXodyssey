0f2324ffeb2aaeb7a1b2061e31fc72b73c5b8d59
Marina <sweet.rinaaa@gmail.com>
2016-03-28 21:41:56 -0500

    admin portal, user auth

--- projects/models.py
+++ projects/models.py
@@ -1,28 +1,18 @@
 from django.db import models
-
-class Student(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=200)
-    def __str__(self):
-        return self.name
-
-class Advisor(models.Model):
-    name = models.CharField(max_length=100)
-    dept = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    def __str__(self):
-        return self.name
+from django.contrib.auth.models import User, Group
+    
 class Project(models.Model):
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=20)
-    advisor = models.ForeignKey(Advisor, on_delete=models.CASCADE)
+    advisor = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.title
-class Group(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    
+class ProjectGroup(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     def __str__(self):
-        return self.student.name
+        return self.project.title+": "+self.student.first_name+" "+self.student.last_name
     class Meta:
         unique_together = (("student", "project"),)
     
--- projects/views.py
+++ projects/views.py
@@ -6,9 +6,6 @@ from django.http import Http404
 from django.views import generic
 from django.template import loader
 
-
-# Create your views here.
-
 def index(request):
     template = loader.get_template('projects/index.html')
     
