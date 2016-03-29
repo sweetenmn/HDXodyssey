d930d6127f2e8b9ac3cd1c8f72413fc29d87d820
Marina <sweet.rinaaa@gmail.com>
2016-03-28 20:58:02 -0500

    add __str__ to models

--- projects/models.py
+++ projects/models.py
@@ -3,22 +3,29 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
 
 class Advisor(models.Model):
     name = models.CharField(max_length=100)
     dept = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
-
+    def __str__(self):
+        return self.name
 class Project(models.Model):
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=20)
     advisor = models.ForeignKey(Advisor, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return self.title
 class Group(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.student.name
     class Meta:
         unique_together = (("student", "project"),)
+    
 
 class Proposal(models.Model):
     project_id = models.OneToOneField(Project, on_delete=models.CASCADE, primary_key=True)
@@ -26,4 +33,6 @@ class Proposal(models.Model):
     created_date = models.DateTimeField('created on')
     status = models.CharField(max_length=20)
     updated_date = models.DateTimeField('updated on')
+    def __str__(self):
+        return self.project_id.title
 
