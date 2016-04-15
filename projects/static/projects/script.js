d40206fb787a0240e6a26a7b449f36ca8ce91111
Elizabeth Dye <dyeea@hendrix.edu>
2016-04-14 20:33:30 -0500

    Trying to create dialogue boxes...

--- projects/static/projects/script.js
+++ projects/static/projects/script.js
@@ -35,10 +35,10 @@ $(document).ready(function(){
     
     function() {
         var dialog = document.getElementById('window');
-        document.getElementById('show').onclick = function() {
+        document.getElementById('#show').onclick = function() {
             dialog.show();
         };
-        document.getElementById('exit').onclick = function() {
+        document.getElementById('#exit').onclick = function() {
             dialog.close();
         };
     };
