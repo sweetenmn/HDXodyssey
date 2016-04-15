2d7dcec2127a6678f5454c497fc883fdd47390ca
Elizabeth Dye <dyeea@hendrix.edu>
2016-04-14 20:37:52 -0500

    popupsssss

--- projects/static/projects/script.js
+++ projects/static/projects/script.js
@@ -33,15 +33,28 @@ $(document).ready(function(){
 		}
 	});
     
-    function() {
-        var dialog = document.getElementById('window');
-        document.getElementById('#show').onclick = function() {
-            dialog.show();
-        };
-        document.getElementById('#exit').onclick = function() {
-            dialog.close();
-        };
-    };
+    $('a.btn-ok, #dialog-overlay, #dialog-box').click(function () {
+        $('#dialog-overlay, #dialog-box').hide();
+        return false;
+    });
+                  
+    // if user resize the window, call the same function again
+    // to make sure the overlay fills the screen and dialogbox aligned to center
+    $(window).resize(function () {
+                                   
+        //only do it if the dialog box is not hidden
+        if (!$('#dialog-box').is(':hidden')) popup();
+    });
+                  
+//    function() {
+//        var dialog = document.getElementById('window');
+//        document.getElementById('#show').onclick = function() {
+//            dialog.show();
+//        };
+//        document.getElementById('#exit').onclick = function() {
+//            dialog.close();
+//        };
+//    };
                   
 	function submit_proposal(){
 		$.ajax({
@@ -109,3 +122,23 @@ $(document).ready(function(){
         }
     });
 });
+
+
+function popup(message) {
+    
+    // get the screen height and width
+    var maskHeight = $(document).height();
+    var maskWidth = $(window).width();
+    
+    // calculate the values for center alignment
+    var dialogTop =  (maskHeight/3) - ($('#dialog-box').height());
+    var dialogLeft = (maskWidth/2) - ($('#dialog-box').width()/2);
+    
+    // assign values to the overlay and dialog box
+    $('#dialog-overlay').css({height:maskHeight, width:maskWidth}).show();
+    $('#dialog-box').css({top:dialogTop, left:dialogLeft}).show();
+    
+    // display the message
+    $('#dialog-message').html(message);
+    
+}
