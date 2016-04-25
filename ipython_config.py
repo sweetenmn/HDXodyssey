54e669ff56acba31d8f42382dd79937bcc9dec0c
My <my@Hoas-MacBook-Air.local>
2016-04-24 23:54:13 -0500

    Added IPython config

---
+++ ipython_config.py
@@ -0,0 +1,10 @@
+python_config.py
+
+c = get_config()
+
+# Allow all IP addresses to use the service and run it on port 8888.
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+
+# Don't load the browser on startup.
+c.NotebookApp.open_browser = False
